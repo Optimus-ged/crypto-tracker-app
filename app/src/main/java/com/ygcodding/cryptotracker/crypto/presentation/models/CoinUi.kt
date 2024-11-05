2e797a0c820a3d12d0f9006c568e2b66943441de
Optimus ged <gedeonyalakuhanda@gmail.com>
2024-11-05 16:58:52 +0200

    linechart created and implemented successfully

--- app/src/main/java/com/ygcodding/cryptotracker/crypto/presentation/models/CoinUi.kt
+++ app/src/main/java/com/ygcodding/cryptotracker/crypto/presentation/models/CoinUi.kt
@@ -4,6 +4,7 @@ import android.icu.text.NumberFormat
 import androidx.annotation.DrawableRes
 import com.ygcodding.cryptotracker.core.presentation.util.getDrawableIdForCoin
 import com.ygcodding.cryptotracker.crypto.domain.model.Coin
+import com.ygcodding.cryptotracker.crypto.presentation.coin_detail.DataPoint
 import java.util.Locale
 
 data class CoinUi(
@@ -14,7 +15,8 @@ data class CoinUi(
     val marketCapUsd: DisplayableNumber,
     val priceUsd: DisplayableNumber,
     val changePercent24Hr: DisplayableNumber,
-    @DrawableRes val iconRes: Int
+    @DrawableRes val iconRes: Int,
+    val coinPriceHistory: List<DataPoint> = emptyList()
 )
 
 data class DisplayableNumber(
